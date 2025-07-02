137dd3e30efb89326398ea7d3c9dc941613bcb48
dennysbarros <dennys.sbarros@gmail.com>
2025-07-02 15:46:40 -0300

    Adding structure and order now test

---
+++ frontend-e2e/integration/login/LoginSelectors.js
@@ -0,0 +1,7 @@
+export const loginSelectors = {
+    signUpWithEmailButton: '[data-testid="login-form"]',
+    emailInput: '[data-testid="email"]',
+    passwordInput: '[data-testid="password"]',
+    submitEmailButton: '[data-testid="submit-email"]',
+    submitFormButton: '[data-testid="submit-form"]'
+}
